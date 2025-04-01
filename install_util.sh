91f382f32ddc540e8311465fc234f90df71f21ea
Dpouris <jimpouris0@gmail.com>
2025-04-01 15:22:00 +0300

    refactor: fix install_util.sh to install regardless if the file is already in local/bin

--- install_util.sh
+++ install_util.sh
@@ -10,7 +10,16 @@ if [ ! -f "$1" ]; then
   exit 1
 fi
 
+set -e
+bin_name=$(basename "$1" .sh)
+if [ -f /usr/local/lib/"$1" ]; then
+  rm /usr/local/lib/"$1"
+fi
+if [ -L /usr/local/bin/"$bin_name" ]; then
+  rm /usr/local/bin/"$bin_name"
+fi
+
 cp "$1" /usr/local/lib/"$1" && \
-ln -s /usr/local/lib/"$1" /usr/local/bin/"$(basename "$1" .sh)" && \
+ln -s /usr/local/lib/"$1" /usr/local/bin/"$bin_name" && \
 echo "Successfully installed \`$1\`"
 
